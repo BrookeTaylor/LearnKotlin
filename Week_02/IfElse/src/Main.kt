bbe7983af40c713b8a41a6cdcaae29ed00632609
Brooke Taylor <anonyoumiss@gmail.com>
2025-11-26 23:46:09 -0600

    adding if else and switch samples

---
+++ Week_02/IfElse/src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val time = 20
+    if (time < 10) {
+        println("Good morning.")
+    } else if (time < 20){
+        println("Good evening.")
+    } else {
+        println("Good evening.")
+    }
+
+}
